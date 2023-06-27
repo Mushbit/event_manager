1ffbc4fce743b01c86c667c18f6a4ac625045f1f
Max <mushbit@protonmail.com>
2023-06-27 12:10:06 +0200

    Finished the write along assignment and will now start with the part that is  unguided.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'erb'
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
@@ -12,14 +13,23 @@ def legislators_by_zipcode(zipcode)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zipcode,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators.officials.map(&:name).join(', ')
+    ).officials
   rescue StandardError
     'You can find your local legislator by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, formletters)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
   end
 end
 
@@ -27,23 +37,22 @@ puts 'Event Manager Initialized!'
 
 puts "Confirm file existence: #{File.exist?('event_attendees.csv')}"
 
-template_letter = File.read('form_letter.erb')
-
 contents = CSV.open(
   'event_attendees.csv',
   headers: true,
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
+  form_letter = erb_template.result(binding)
 
-  puts personal_letter
+  save_thank_you_letter(id, form_letter)
 end
