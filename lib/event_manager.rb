63d6557561220cf53cad4f57c086c9ae43c202fe
Max <mushbit@protonmail.com>
2023-07-06 11:35:37 +0200

    Learned how to parse and format time and date. Bit confusing and since it wont be used all the time, I'm sure I'll look this up on multiple occasions."

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -19,6 +19,10 @@ def clean_phone_number(phone_number)
   end
 end
 
+def time_target(reg_hours)
+  reg_hours.sum / reg_hours.length
+end
+
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -44,17 +48,6 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
-day = []
-hour = []
-
-def split_regestration_date(regdate)
-  split_reg_date = regdate.split(" ")
-  date = Time.strptime(split_reg_date[0], "%m/%d/%y")
-  # Just figured out that Time class can be used with strptime and now want to "format" the Time object to output the day of the week.
-  day << date.strftime("%a")
-  hour << Time.parse(split_reg_date[1]).hour
-end
-
 puts 'Event Manager Initialized!'
 
 puts "Confirm file existence: #{File.exist?('event_attendees.csv')}"
@@ -68,17 +61,19 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+reg_hours = []
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   phone_number = clean_phone_number(row[:homephone])
-  puts zipcode = clean_zipcode(row[:zipcode])
-  reg_date = split_regestration_date(row[:regdate])
+  zipcode = clean_zipcode(row[:zipcode])
+  puts reg_date = Time.strptime(row[:regdate], "%m/%d/%y %k:%M")
+  reg_hours << reg_date.hour
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
   #save_thank_you_letter(id, form_letter)
 end
 
-p day
-p hour
+puts "The best time to have our advertisments up is between #{time_target(reg_hours) - 1} and #{time_target(reg_hours) + 1}"
